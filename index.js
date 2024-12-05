2205a20223df3cebbe73ecfbb5f239c97a4f3737
nadeemk2023 <nadeemekhan@gmail.com>
2024-12-04 22:52:33 -0800

    Comment out fetch implementation and add note for Axios GET request

--- index.js
+++ index.js
@@ -20,16 +20,18 @@
 //   });
 
 // TASK: fetch a list of posts from JSONplaceholder API
-fetch(`https://jsonplaceholder.typicode.com/posts`)
-  .then(response => {
-    if (!response.ok) {
-      throw new Error('Network response was not ok', response.status);
-    }
-    return response.json();
-  })
-  .then(posts => {
-    posts.forEach(post => console.log('Post title:', post.title));
-  })
-  .catch(error => {
-    console.error('There was an error retrieving the posts:', error);
-  });
+// fetch(`https://jsonplaceholder.typicode.com/posts`)
+//   .then(response => {
+//     if (!response.ok) {
+//       throw new Error('Network response was not ok', response.status);
+//     }
+//     return response.json();
+//   })
+//   .then(posts => {
+//     posts.forEach(post => console.log('Post title:', post.title));
+//   })
+//   .catch(error => {
+//     console.error('There was an error retrieving the posts:', error);
+//   });
+
+// GET Request with Axios
