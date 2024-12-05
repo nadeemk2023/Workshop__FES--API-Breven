49292c34307c9240ca327f99ddfa91ce058b851b
nadeemk2023 <nadeemekhan@gmail.com>
2024-12-04 22:35:35 -0800

    Add example fetch requests to index.js for reference GET

---
+++ index.js
@@ -0,0 +1,20 @@
+// BASIC SYNTAX
+// fetch('https://jsonplaceholder.typicode.com/posts')
+//   .then(response => response.json())
+//   .then(data => console.log(data.slice(0, 6)))
+//   .catch(error => console.log(error));
+
+// GET REQUEST with fetch
+// fetch('https://jsonplaceholder.typicode.com/posts')
+//   .then(response => {
+//     if (!response.ok) {
+//       throw new Error('Network response was not ok:' + response.status);
+//     }
+//     return response.json();
+//   })
+//   .then(data => {
+//     console.log('Data received: ', data);
+//   })
+//   .catch(error => {
+//     console.error('There was a problem with the fetch operation:', error);
+//   });
